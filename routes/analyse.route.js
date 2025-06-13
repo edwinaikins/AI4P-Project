eeff07952192d2199615904cda3fc29fc3f9484d
gfrimp <150941247+gfrimp@users.noreply.github.com>
2025-06-13 08:33:46 +0000

    updated technical feasibility endpoint.

--- routes/analyse.route.js
+++ routes/analyse.route.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { analyseClarityandCoherence, analyseCreativity, analyseEthicalEvaluation, analyseImpactAssessment } from '../controllers/analyse.controller.js';
+import { analyseClarityandCoherence, analyseCreativity, analyseEthicalEvaluation, analyseImpactAssessment, analyseTechnicalFeasibility } from '../controllers/analyse.controller.js';
 
 const router = Router();
 
 router.post('/analysecreativity', analyseCreativity);
-router.post('/analysetechnicalfeasibility', analyseCreativity);
+router.post('/analysetechnicalfeasibility', analyseTechnicalFeasibility);
 router.post('/analyseimpactassessment', analyseImpactAssessment);
 router.post('/analyseethicalevaluation', analyseEthicalEvaluation);
 router.post('/analyseclarityandcoherence', analyseClarityandCoherence);
