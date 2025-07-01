fc417bf56fae1bb6c4666e0612fdf141979e947d
Gfrim <84197818+Gfrim@users.noreply.github.com>
2025-07-01 11:12:43 +0000

    Update index.js
    Changed model to optimize and reduce delay in response.

--- config/index.js
+++ config/index.js
@@ -5,5 +5,5 @@ export const config = {
   PORT: process.env.PORT || 8080,
   PROJECT_ID: process.env.GCP_PROJECT_ID,
   LOCATION: 'global',
-  MODEL: 'gemini-2.5-pro-preview-06-05',
+  MODEL: 'gemini-1.5-flash', //'gemini-2.5-pro-preview-06-05',
 };
