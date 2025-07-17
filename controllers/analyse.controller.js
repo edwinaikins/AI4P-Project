31c5bb3dde52b10680e7ff8b1513803a17ca4a23
Gfrim <84197818+Gfrim@users.noreply.github.com>
2025-07-17 12:44:11 +0000

    Update analyse.controller.js

--- controllers/analyse.controller.js
+++ controllers/analyse.controller.js
@@ -100,7 +100,7 @@ export const analyseClarityandCoherence = async (req, res) => {
 }
 
 export const analyseFullIdea = async (req, res) => {
-  const { new_idea, challenge } = req.body;
+  const { new_idea, challenge, author_id } = req.body;
 
   if (typeof new_idea !== 'string' || new_idea.trim() === '') {
     return res.status(400).json({ error: 'Invalid or empty idea provided.' });
