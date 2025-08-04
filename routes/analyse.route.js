af4a38e7ba97173b2b5cfd5ff775b30f51910e01
Gfrim <84197818+Gfrim@users.noreply.github.com>
2025-08-04 09:07:58 +0000

    Update analyse.route.js

--- routes/analyse.route.js
+++ routes/analyse.route.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
-import { analyseClarityandCoherence, analyseCreativity, analyseEthicalEvaluation, analyseImpactAssessment, analyseTechnicalFeasibility, analyseExtractIdea, analyseFullIdea, analyseCreativityIdea, analyseStackRanking } from '../controllers/analyse.controller.js';
+import { analyseClarityandCoherence, analyseCreativity, analyseEthicalEvaluation, analyseImpactAssessment, analyseTechnicalFeasibility, analyseExtractIdea, analyseFullIdea, analyseCreativityIdea, analyseStackRanking, stackranking } from '../controllers/analyse.controller.js';
 
 const router = Router();
 const upload = multer({ dest: 'uploads/' });
@@ -14,5 +14,6 @@ router.post('/analyseclarityandcoherence', analyseClarityandCoherence);
 router.post('/analysefullidea', analyseFullIdea);
 router.post('/extractidea', upload.single('file'), analyseExtractIdea);
 router.post('/stackranking', analyseStackRanking);
+router.post('/testranking', stackranking);
 
 export default router;
